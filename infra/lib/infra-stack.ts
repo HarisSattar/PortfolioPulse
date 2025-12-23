6d9ff707f9356c459d776c563119ea34ffd655c2
Haris Sattar <harissattar41@gmail.com>
2025-12-22 17:20:11 -0800

    test automated deployment

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,16 +1,44 @@
 import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as path from 'path';
 
-export class InfraStack extends cdk.Stack {
+export class PortfolioPulseStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // 1. The Bucket to hold your code
+    const websiteBucket = new s3.Bucket(this, 'PortfolioPulseBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL, // Security best practice
+    });
+
+    // 2. The CloudFront distribution (The URL users visit)
+    const distribution = new cloudfront.Distribution(this, 'PortfolioPulseDist', {
+      defaultBehavior: { 
+        origin: origins.S3BucketOrigin.withOriginAccessControl(websiteBucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      defaultRootObject: 'index.html',
+    });
+
+    // 3. Deployment: Sync the Next.js 'out' folder to S3
+    new s3deploy.BucketDeployment(this, 'DeployWebsite', {
+      sources: [s3deploy.Source.asset(path.join(__dirname, '../../out'))],
+      destinationBucket: websiteBucket,
+      distribution,
+      distributionPaths: ['/*'], // Clears the cache on update
+    });
+
+    // Output the URL to your terminal
+    new cdk.CfnOutput(this, 'WebsiteURL', {
+      value: distribution.domainName,
+    });
   }
 }
